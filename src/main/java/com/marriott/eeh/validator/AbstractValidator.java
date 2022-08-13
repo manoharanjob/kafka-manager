85553b40bad7a7f2420df277f0dac87cd3195638
manoharanjob <manoharanjob@gmail.com>
2022-08-13 11:08:32 +0530

    Update AbstractValidator.java

--- src/main/java/com/marriott/eeh/validator/AbstractValidator.java
+++ src/main/java/com/marriott/eeh/validator/AbstractValidator.java
@@ -2,4 +2,16 @@ package com.marriott.eeh.validator;
 
 public class AbstractValidator {
 
+  public void replaceTemplate(ConstraintValidatorContext context, String templateKey) {
+        HibernateConstraintValidatorContext validator = context.unwrap(HibernateConstraintValidatorContext.class);
+        validator.disableDefaultConstraintViolation();
+        validator.buildConstraintViolationWithTemplate(templateKey).addConstraintViolation();
+    }
+
+    public void replaceTemplate(ConstraintValidatorContext context, String templateKey, String paramName, String paramValue) {
+        HibernateConstraintValidatorContext validator = context.unwrap(HibernateConstraintValidatorContext.class);
+        validator.disableDefaultConstraintViolation();
+        validator.addMessageParameter(paramName, paramValue).buildConstraintViolationWithTemplate(templateKey).addConstraintViolation();
+    }
+  
 }
