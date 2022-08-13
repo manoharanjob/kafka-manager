f65849de31a9b462c2b6c28d0b2607770701092d
manoharanjob <manoharanjob@gmail.com>
2022-08-13 10:59:29 +0530

    Update NameValidator.java

--- src/main/java/com/marriott/eeh/validator/NameValidator.java
+++ src/main/java/com/marriott/eeh/validator/NameValidator.java
@@ -1,6 +1,42 @@
 package com.marriott.eeh.validator;
 
-public class NameValidator {
+import com.pwc.bcm.core.validators.constraint.EmailFormat;
+import com.pwc.bcm.core.validators.util.ValidationUtil;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import static com.pwc.bcm.core.validators.constants.ValidationErrorConstants.INVALID_EMAIL_FORMAT;
+
+public class EmailFormatValidator implements ConstraintValidator<EmailFormat,String> {
 
 
+    private boolean exists;
+
+    /**
+     * The method will initialize the annotated element
+     *
+     * @param constraintAnnotation the EventId annotation
+     */
+    @Override
+    public void initialize(EmailFormat constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    /**
+     * The method handle a validation logic for the annotated element
+     *
+     * @param value   the EmailFormat
+     * @param context the constraint validator constraintValidatorContext
+     * @return boolean true|false
+     */
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        if (email.matches("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" +
+                "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")){
+            return true;
+        }
+        ValidationUtil.replaceTemplate(constraintValidatorContext,INVALID_EMAIL_FORMAT);
+        return false;
+    }
 }
